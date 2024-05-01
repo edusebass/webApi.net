6b1fa979d7c04a804ce6e727d25cc0160eadffc4
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-05-01 17:47:02 -0500

    services

--- Services/TareasService.cs
+++ Services/TareasService.cs
@@ -14,9 +14,43 @@ public class TareasService : ITareasService
     {
         return context.Tareas;
     }
+
+    public async Task Save(Tarea tarea)
+    {
+        context.Add(tarea);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task Update(Guid id, Tarea tarea)
+    {
+        var tareaActual = context.Tareas.Find(id);
+
+        if (tareaActual == null)
+        {
+            tareaActual.Titulo = tarea.Titulo;
+            tareaActual.Resumen = tarea.Resumen;
+            tareaActual.FechaCreacion = tarea.FechaCreacion;
+            tareaActual.Descripcion = tarea.Descripcion;
+            await context.SaveChangesAsync();
+        }
+    }
+
+    public async Task Delete(Guid id)
+    {
+        var tareaActual = context.Tareas.Find(id);
+
+        if (tareaActual == null)
+        {
+            context.Remove(tareaActual);
+            await context.SaveChangesAsync();
+        }
+    }
 }
 
 public interface ITareasService
 {
     IEnumerable<Tarea> Get();
+    Task Save(Tarea tarea);
+    Task Update(Guid id, Tarea tarea);
+    Task Delete(Guid id);
 }
